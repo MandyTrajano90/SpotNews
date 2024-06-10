196ac2698e97e7616fd2d1fb71cc388e6dc15096
Mandytrajano90 <mandytrajano90@gmail.com>
2024-06-10 11:59:04 -0300

    Reqs 4 and 5 - Home and News Details templates

--- news/views.py
+++ news/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import News
 
-# Create your views here.
+
+def home(request):
+    content = {"news": News.objects.all()}
+    return render(request, "home.html", content)
+
+
+def news_details(request, id):
+    content = {"news": News.objects.get(id=id)}
+    return render(request, 'news_details.html', content)
